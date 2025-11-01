c02995d7627ce42405da06091687ac2bec3e7f96
Mio <mio@mio19.uk>
2025-11-01 16:29:46 +1100

    update hash; nixfmt

--- flake.nix
+++ flake.nix
@@ -27,9 +27,7 @@
             zen-browser-unwrapped = pkgs.callPackage ./zen-browser-unwrapped.nix {
               inherit version firefoxVersion;
             };
-            zen-browser = builtins.warn ''
-              This flake is deprecated, please use https://github.com/youwen5/zen-browser-flake.
-            '' (pkgs.callPackage ./zen-browser.nix { inherit zen-browser-unwrapped; });
+            zen-browser = (pkgs.callPackage ./zen-browser.nix { inherit zen-browser-unwrapped; });
             default = zen-browser;
           };
         };
--- zen-browser-unwrapped.nix
+++ zen-browser-unwrapped.nix
@@ -190,139 +190,136 @@ buildStdenv.mkDerivation (finalAttrs: {
   inherit firefoxVersion;
   firefoxSrc = fetchurl {
     url = "mirror://mozilla/firefox/releases/${finalAttrs.firefoxVersion}/source/firefox-${finalAttrs.firefoxVersion}.source.tar.xz";
-    hash = "sha256-XAMbVywdpyZnfi/5e2rVp+OyM4em/DljORy1YvgKXkg=";
+    hash = "sha256-6sRyLtJZAI1zAGxIlMGNKHFwLGYdFOJ1BYEjUd9igGs=";
   };
 
   SURFER_COMPAT = generic;
 
-  nativeBuildInputs =
-    [
-      autoconf
-      cargo
-      git
-      gnum4
-      llvmPackagesBuildBuild.bintools
-      makeWrapper
-      nasm
-      nodejs
-      pkg-config
-      python3
-      rsync
-      rust-cbindgen
-      rustPlatform.bindgenHook
-      rustc
-      surfer
-      unzip
-      wrapGAppsHook3
-      xorg.xvfb
-    ]
-    ++ lib.optionals crashreporterSupport [
-      dump_syms
-      patchelf
-    ];
+  nativeBuildInputs = [
+    autoconf
+    cargo
+    git
+    gnum4
+    llvmPackagesBuildBuild.bintools
+    makeWrapper
+    nasm
+    nodejs
+    pkg-config
+    python3
+    rsync
+    rust-cbindgen
+    rustPlatform.bindgenHook
+    rustc
+    surfer
+    unzip
+    wrapGAppsHook3
+    xorg.xvfb
+  ]
+  ++ lib.optionals crashreporterSupport [
+    dump_syms
+    patchelf
+  ];
 
-  buildInputs =
-    [
-      atk
-      cairo
-      cups
-      dbus
-      dbus-glib
-      ffmpeg
-      fontconfig
-      freetype
-      gdk-pixbuf
-      gtk3
-      glib
-      icu73
-      libGL
-      libGLU
-      libevent
-      libffi
-      libglvnd
-      libjpeg
-      libnotify
-      libpng
-      libstartup_notification
-      libva
-      libvpx
-      libwebp
-      libxml2
-      mesa
-      nspr
-      nss_latest
-      pango
-      pciutils
-      pipewire
-      udev
-      xcb-util-cursor
-      xorg.libX11
-      xorg.libXcursor
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXft
-      xorg.libXi
-      xorg.libXrender
-      xorg.libXt
-      xorg.libXtst
-      xorg.pixman
-      xorg.xorgproto
-      xorg.libxcb
-      xorg.libXrandr
-      xorg.libXcomposite
-      xorg.libXfixes
-      xorg.libXScrnSaver
-      zlib
-    ]
-    ++ lib.optional alsaSupport alsa-lib
-    ++ lib.optional jackSupport libjack2
-    ++ lib.optional pulseaudioSupport libpulseaudio
-    ++ lib.optional sndioSupport sndio
-    ++ lib.optional gssSupport libkrb5
-    ++ lib.optional jemallocSupport jemalloc
-    ++ lib.optionals waylandSupport [
-      libdrm
-      libxkbcommon
-    ];
+  buildInputs = [
+    atk
+    cairo
+    cups
+    dbus
+    dbus-glib
+    ffmpeg
+    fontconfig
+    freetype
+    gdk-pixbuf
+    gtk3
+    glib
+    icu73
+    libGL
+    libGLU
+    libevent
+    libffi
+    libglvnd
+    libjpeg
+    libnotify
+    libpng
+    libstartup_notification
+    libva
+    libvpx
+    libwebp
+    libxml2
+    mesa
+    nspr
+    nss_latest
+    pango
+    pciutils
+    pipewire
+    udev
+    xcb-util-cursor
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXft
+    xorg.libXi
+    xorg.libXrender
+    xorg.libXt
+    xorg.libXtst
+    xorg.pixman
+    xorg.xorgproto
+    xorg.libxcb
+    xorg.libXrandr
+    xorg.libXcomposite
+    xorg.libXfixes
+    xorg.libXScrnSaver
+    zlib
+  ]
+  ++ lib.optional alsaSupport alsa-lib
+  ++ lib.optional jackSupport libjack2
+  ++ lib.optional pulseaudioSupport libpulseaudio
+  ++ lib.optional sndioSupport sndio
+  ++ lib.optional gssSupport libkrb5
+  ++ lib.optional jemallocSupport jemalloc
+  ++ lib.optionals waylandSupport [
+    libdrm
+    libxkbcommon
+  ];
 
-  configureFlags =
-    [
-      "--disable-bootstrap"
-      "--disable-updater"
-      "${disableAVX}"
-      "--enable-default-toolkit=cairo-gtk3${lib.optionalString waylandSupport "-wayland"}"
-      "--enable-system-pixman"
-      "--with-distribution-id=org.nixos"
-      "--with-libclang-path=${llvmPackagesBuildBuild.libclang.lib}/lib"
-      "--with-system-ffi"
-      "--with-system-icu"
-      "--with-system-jpeg"
-      "--with-system-libevent"
-      "--with-system-libvpx"
-      "--with-system-nspr"
-      "--with-system-nss"
-      "--with-system-png" # needs APNG support
-      "--with-system-webp"
-      "--with-system-zlib"
-      "--with-wasi-sysroot=${wasiSysRoot}"
-      "--host=${buildStdenv.buildPlatform.config}"
-      "--target=${buildStdenv.hostPlatform.config}"
-    ]
-    ++ [
-      (lib.enableFeature alsaSupport "alsa")
-      (lib.enableFeature ffmpegSupport "ffmpeg")
-      (lib.enableFeature geolocationSupport "necko-wifi")
-      (lib.enableFeature gssSupport "negotiateauth")
-      (lib.enableFeature jackSupport "jack")
-      (lib.enableFeature jemallocSupport "jemalloc")
-      (lib.enableFeature pulseaudioSupport "pulseaudio")
-      (lib.enableFeature sndioSupport "sndio")
-      (lib.enableFeature webrtcSupport "webrtc")
-      # --enable-release adds -ffunction-sections & LTO that require a big amount
-      # of RAM, and the 32-bit memory space cannot handle that linking
-      (lib.enableFeature (!debugBuild && !stdenv.hostPlatform.is32bit) "release")
-      (lib.enableFeature enableDebugSymbols "debug-symbols")
-    ];
+  configureFlags = [
+    "--disable-bootstrap"
+    "--disable-updater"
+    "${disableAVX}"
+    "--enable-default-toolkit=cairo-gtk3${lib.optionalString waylandSupport "-wayland"}"
+    "--enable-system-pixman"
+    "--with-distribution-id=org.nixos"
+    "--with-libclang-path=${llvmPackagesBuildBuild.libclang.lib}/lib"
+    "--with-system-ffi"
+    "--with-system-icu"
+    "--with-system-jpeg"
+    "--with-system-libevent"
+    "--with-system-libvpx"
+    "--with-system-nspr"
+    "--with-system-nss"
+    "--with-system-png" # needs APNG support
+    "--with-system-webp"
+    "--with-system-zlib"
+    "--with-wasi-sysroot=${wasiSysRoot}"
+    "--host=${buildStdenv.buildPlatform.config}"
+    "--target=${buildStdenv.hostPlatform.config}"
+  ]
+  ++ [
+    (lib.enableFeature alsaSupport "alsa")
+    (lib.enableFeature ffmpegSupport "ffmpeg")
+    (lib.enableFeature geolocationSupport "necko-wifi")
+    (lib.enableFeature gssSupport "negotiateauth")
+    (lib.enableFeature jackSupport "jack")
+    (lib.enableFeature jemallocSupport "jemalloc")
+    (lib.enableFeature pulseaudioSupport "pulseaudio")
+    (lib.enableFeature sndioSupport "sndio")
+    (lib.enableFeature webrtcSupport "webrtc")
+    # --enable-release adds -ffunction-sections & LTO that require a big amount
+    # of RAM, and the 32-bit memory space cannot handle that linking
+    (lib.enableFeature (!debugBuild && !stdenv.hostPlatform.is32bit) "release")
+    (lib.enableFeature enableDebugSymbols "debug-symbols")
+  ];
 
   configureScript = writeShellScript "configureMozconfig" ''
     ${
